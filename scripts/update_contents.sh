f7235a0987d8579c445f9e543dfa046965c207c4
Jason Benn <jasoncbenn@gmail.com>
2021-02-18 10:23:26 -0800

    Quiet script, full paths so it works from cron

--- scripts/update_contents.sh
+++ scripts/update_contents.sh
@@ -1,5 +1,5 @@
-(cd /home/flock/roam-notes && git fetch && git reset --hard origin/master)
-python scripts/clean.py
+(cd /home/flock/roam-notes && git fetch && git reset --hard origin/master --quiet)
+/home/flock/venvs/worldview/bin/python /home/flock/notes-garden/scripts/clean.py
 if [ -n "$(git status --porcelain)" ]; then
   git add .
   git commit -m "Updated notes"
