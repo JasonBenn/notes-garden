b594646c0bcdaeec30df4fb30cf2fbfb5723178a
Jason Benn <jasoncbenn@gmail.com>
2021-03-29 16:39:10 +0000

    Updated notes

--- scripts/update_contents.sh
+++ scripts/update_contents.sh
@@ -1,3 +1,7 @@
+# redirect output to syslog
+# https://www.urbanautomaton.com/blog/2014/09/09/redirecting-bash-script-output-to-syslog/
+exec 1> >(logger -s -t $(basename $0)) 2>&1
+
 (cd /home/flock/roam-notes && git fetch && git reset --hard origin/master --quiet)
 /home/flock/venvs/worldview/bin/python /home/flock/notes-garden/scripts/clean.py
 if [ -n "$(git status --porcelain)" ]; then
